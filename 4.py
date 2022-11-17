55395a514cec0ee07feeedac80151d1107084944
Darya Timohovich <timahovich.dm@gmail.com>
2022-11-17 20:29:10 +0300

    new

--- 4.py
+++ 4.py
@@ -2,3 +2,20 @@ from random import randint
 a = (1, 18, 55, 6, 78, 99, 78, 85, 12, 22 )
 print(max(a))
 print(min(a))
+
+
+b = []
+c = []
+for i in range(10):
+    k = randint(0, 5)
+    b.append(randint(0, 5))
+    c.append(randint(0, 5))
+b = tuple(b)
+c = tuple(c)
+print(b)
+print(c)
+d = b+c
+print(d)
+print(d.count(0))
+
+
